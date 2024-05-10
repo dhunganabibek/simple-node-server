74cf59385535ec19f621a7103447293bcfb913ed
Bibek Dhungana <51139940+dhunganabibek@users.noreply.github.com>
2024-05-10 00:00:52 -0500

    test (#2)
    * all js file deleted
    
    * working with writableLength and writableHighWaterMark in steram
    
    * WIP

--- stream/writeMany.js
+++ stream/writeMany.js
@@ -1,17 +1,12 @@
 import { open } from "node:fs/promises";
-
 const timeStart = performance.now();
-
 const fileHandler = await open("test.txt", "w");
-
 const writeStream = fileHandler.createWriteStream();
 console.log(writeStream.writableHighWaterMark);
-
 writeStream.write("H");
 const buffer = Buffer.alloc(16384, "a");
 console.log(writeStream.write(buffer));
 console.log(writeStream.writableLength);
-
 fileHandler.close();
 const timeEnd = performance.now();
 console.log("Execution time: ", timeEnd - timeStart, "ms");
