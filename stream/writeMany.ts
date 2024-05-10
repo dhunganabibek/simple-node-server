82ab88658f9d77d3114ea36723bf017c48b77d55
Bibek Dhungana <dhunganabibek2016@gmail.com>
2024-05-09 23:22:32 -0500

    working with file write stream

--- stream/writeMany.ts
+++ stream/writeMany.ts
@@ -4,10 +4,8 @@ const timeStart = performance.now();
 
 const fileHandler = await open("test.txt", "w");
 
-for (let i = 0; i < 1000000; i++) {
-  await fileHandler.write(`Hello World ${i}\n`);
-}
-
+const writeStream = fileHandler.createWriteStream();
+writeStream.write("Hello World\n".repeat(1000000));
 fileHandler.close();
 const timeEnd = performance.now();
 console.log("Execution time: ", timeEnd - timeStart, "ms");
