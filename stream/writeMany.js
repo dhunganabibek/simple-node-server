b8a597349c30e2c8b0078e75349947bc87e307f2
Bibek Dhungana <dhunganabibek2016@gmail.com>
2024-05-09 23:36:21 -0500

    all js file deleted

--- stream/writeMany.js
+++
@@ -1,8 +0,0 @@
-import { open } from "node:fs/promises";
-const timeStart = performance.now();
-const fileHandler = await open("test.txt", "w");
-const writeStream = fileHandler.createWriteStream();
-writeStream.write("Hello World\n".repeat(1000000));
-fileHandler.close();
-const timeEnd = performance.now();
-console.log("Execution time: ", timeEnd - timeStart, "ms");
